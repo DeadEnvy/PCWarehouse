37a2ded5b36fffd5e720c2ac07d669b96528ae81
Marcus <Marcus.mamott@gmail.com>
2018-11-07 15:36:42 -0600

    Implemented the index and show Products controller actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
   def index
+    @products_collection = Product.order(:name).all
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index, :show]
+  root to: 'product#index'
 end
