5c0ce4ed6a2bec7a633bdb8c969375b3f3935a48
Marcus Mamott <Marcus.Mamott@gmail.com>
2018-11-07 21:09:56 +0000

    Installed active admin

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
     has_many :product_categories
-    has_many :categories through :product_category
+    has_many :categories, through: :product_categories
     has_many :orders
 end
