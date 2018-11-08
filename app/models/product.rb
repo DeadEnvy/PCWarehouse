61c8bcfc55b83d5aeb62b1d410518f6037d14a5f
Marcus <Marcus.mamott@gmail.com>
2018-11-08 10:50:01 -0600

    Mounted image uploader.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,7 @@ class Product < ApplicationRecord
     has_many :orders
 
     validates :name, presence: true
-    validates :price, numericality: { greater_than: 0 }
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+    mount_uploader :image, ImageUploader
 end
