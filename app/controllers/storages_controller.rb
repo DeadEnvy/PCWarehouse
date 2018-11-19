f5d975ae8ced1d37370f4f6c75d952a97ef46587
Marcus <Marcus.mamott@gmail.com>
2018-11-18 21:26:26 -0600

    Created a review page with file upload

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -3,7 +3,7 @@ class StoragesController < InheritedResources::Base
   private
 
     def storage_params
-      params.require(:storage).permit(:title, :description)
+      params.require(:storage).permit(:title, :description, :file)
     end
 end
 
